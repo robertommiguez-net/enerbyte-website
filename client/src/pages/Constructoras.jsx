f50dbab68b5dff91df4333976ebbd668707d3a4c
robertommiguez-net <roberto.m.miguez@gmail.com>
2026-06-30 20:19:26 -0300

    up

--- client/src/pages/Constructoras.jsx
+++ client/src/pages/Constructoras.jsx
@@ -1,4 +1,5 @@
-export default function Constructoras() {
+export default function
+ Constructoras() {
   return (
     <div style={{ minHeight: "100vh", background: "white", color: "black", padding: "40px" }}>
       <h1>Página Constructoras funcionando</h1>
